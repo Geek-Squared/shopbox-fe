303a9fff51f45dfd31f65d44a013a94fb5ed39f6
Geek-Squared <your-geek-squared-email@example.com>
2026-03-26 12:00:03 +0200

    next update

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 import { createRequestHandler } from "@react-router/express";
 import * as build from "../build/server/index.js";
 
-export default createRequestHandler({
-  build,
-  mode: process.env.NODE_ENV,
-});
+const handler = createRequestHandler({ build, mode: process.env.NODE_ENV });
+
+export default async function (req, res) {
+  await handler(req, res, () => {});
+}
